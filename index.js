ab7c3dffe3ee1f513fd30c0ec1e21f988f1eaaf2
mlukjaniuk <ma.lukjaniuk@gmail.com>
2023-07-10 09:04:11 +0200

    functions getting specific titles and ratings

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const axios = require('axios');
-const cheerio = require('cheerio');
-
+const jsdom = require("jsdom")
 
 const url = 'https://www.filmweb.pl/ranking/vod';
 
@@ -31,13 +30,51 @@ const disney_plus = {
 async function getData(vod_url) {
     try {
         const data = await axios.get(url + vod_url + '/2023');
-        return data;
+        return data.data;
     } catch (error) {
         console.error(error);
       }
 };
 
+function getTitles(document) {
+    const dom = new jsdom.JSDOM(document);
+    document = dom.window.document;
+    const moviesTags = document.querySelectorAll('a[href^="/film/"]');
+    const moviesArray = Array.from(moviesTags);
+    const moviesTitles = moviesArray.map(movie => movie.textContent).filter(movie => movie !== '');
+    return moviesTitles.splice(0, 10);
+}
+
+function getRatings(document) {
+    const dom = new jsdom.JSDOM(document);
+    document = dom.window.document;
+    const ratingsTags = document.querySelectorAll('span[class="rankingType__rate--value"]');
+    const ratingsArray = Array.from(ratingsTags);
+    const ratings = ratingsArray.map(rating => rating.textContent)
+    return ratings.splice(0, 10);
+}
+
+async function addMovies(vod) {
+    await getData(vod.url).then(res => {
+        titles = getTitles(res);
+        ratings = getRatings(res);
+        titles.forEach((title, index) => vod.movies.push({title: title, rating: ratings[index]}));
+    })
+}
+
+    
 
 // getData(netflix.url).then(res => {
-//     console.log(res)
+//     addMovies(netflix).then(() => {
+//         addMovies(hbo).then(() => {
+//             addMovies(canal_plus).then(() => {
+//                 addMovies(disney_plus).then(() => {
+//                     console.log(netflix);
+//                     console.log(hbo);
+//                     console.log(canal_plus);
+//                     console.log(disney_plus);
+//                 })
+//             })
+//         })
+//     })
 // })
